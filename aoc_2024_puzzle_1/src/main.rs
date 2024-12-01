0b6fd3e6759a7ca236871a9117edacd11bf82ffd
Logimancer <the.logimancer@gmail.com>
2024-12-01 18:27:02 -0500

    First attempt at puzzle 1 answer

--- aoc_2024_puzzle_1/src/main.rs
+++ aoc_2024_puzzle_1/src/main.rs
@@ -1,5 +1,11 @@
 use std::{ env, fs::File, io::{ self, BufRead }, path::Path };
 
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
 struct List<T> {
     column_1: Vec<T>,
     column_2: Vec<T>, 
@@ -12,6 +18,35 @@ impl<T> List<T> {
         }
     }
 }
+
+impl List<i64>{
+    fn process_txt_file(&mut self, file_path: &String) {
+        if let Ok(lines) = read_lines(file_path) {
+            for line in lines.flatten() {
+                //would have used collect to tupple in itertools, but wanted to keep it std :P    
+                let numbers: Vec<&str> = line.split_whitespace().collect(); 
+                self.column_1.push(numbers[0].parse().unwrap());
+                self.column_2.push(numbers[1].parse().unwrap());
+            }
+        }
+    }
+
+    fn order(&mut self) {
+        self.column_1.sort_unstable();
+        self.column_2.sort_unstable();
+    }
+
+    //TODO: Make this immutable and more idiomatic
+    fn difference(mut self) -> Vec<u64> {
+        let mut differences = Vec::new();
+        while !self.column_1.is_empty() & !self.column_2.is_empty() {
+            let column_1 = self.column_1.pop().unwrap();
+            let column_2 = self.column_2.pop().unwrap();
+            differences.push(column_1.abs_diff(column_2));
+        };
+        differences
+    }
+}
  
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -21,31 +56,16 @@ fn main() {
     let mut list: List<i64> = List::new();
 
     //create list of u64 numbers
-    if let Ok(lines) = read_lines(file_path) {
-        for line in lines.flatten() {
-            let numbers: Vec<&str> = line.split_whitespace().collect(); //would have used collect to tupple in itertools, but wanted to keep it std :P
-            list.column_1.push(numbers[0].parse().unwrap());
-            list.column_2.push(numbers[1].parse().unwrap());
-        }
-    }
+    list.process_txt_file(file_path);
 
     //order lists
-    list.column_1.sort_unstable();
-    list.column_2.sort_unstable();
+    list.order();
 
-    //do arithmatic on each number
-    
-    while !list.column_1.is_empty() & !list.column_2.is_empty() {
-        let column_1 = list.column_1.pop().unwrap();
-        let column_2 = list.column_2.pop().unwrap();
-        let differnce = column_1.abs_diff(column_2);
-        println!("The difference between columnt_1 ({}) and column_2 ({}) is: {}", column_1, column_2, differnce);
-    };
+    //compute differences of each line
+    let differences = list.difference();
 
-}
+    //add every number in vector
+    let answer: u64 = differences.iter().sum();
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+    println!("Puzzle 1 Answer: {}", answer);
 }
